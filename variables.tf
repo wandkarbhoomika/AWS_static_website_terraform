06c8c18a781c3c014f0f56a82c6af7fbdf2b451e
wandkarbhoomika <wandkarbhoomika@gmail.com>
2026-05-11 10:34:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
 variable "aws_region" {
-  default = "ap-south-1"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
 }
+
+variable "bucket_name" {
+  description = "S3_Bucket_Static_Website"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "Dev"
+}
+  
