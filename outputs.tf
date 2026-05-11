d66e3e8f282ac6c4ab0818a88cd89d7041ce3063
wandkarbhoomika <wandkarbhoomika@gmail.com>
2026-05-11 10:36:04 +0530

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "website_url" {
-  value = "http://${aws_s3_bucket.website_bucket.bucket}.s3-website.ap-south-1.amazonaws.com"
+  description = "Public URL of the S3 static website"
+  value       = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
+}
+
+output "bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.website_bucket.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.website_bucket.arn
 }
